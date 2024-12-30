417499e442c6428a63a5b9e4ab5abcb682e0c35c
PrakarshSrivastav <prafir10@gmail.com>
2024-12-30 12:44:46 +0100

    Corrected error which I made

--- src/main/java/com/prakarsh/Bank_App/Account.java
+++ src/main/java/com/prakarsh/Bank_App/Account.java
@@ -1,9 +1,6 @@
 package com.prakarsh.Bank_App;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -15,7 +12,7 @@ public class Account {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    @Column(name = "accountHolderName")
     private String accountHolderName;
     private double balance;
 
